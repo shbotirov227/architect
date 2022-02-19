c9e99056e426bbf57cd31931989422eb17f55fef
shbotirov <shbotirov227@gmail.com>
2022-02-19 14:06:32 +0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Main from "./Main/Main";
+import Gallery from "./Gallery/Gallery";
+import Projects from "./Projects/Projects";
+import Certification from "./Certification/Certification";
+import Contacts from "./Contacts/Contacts";
+
+export { Main, Gallery, Projects, Certification, Contacts };
